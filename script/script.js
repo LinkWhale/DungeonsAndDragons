e039034d0a889bdfb46962841f9bbca3d7b35f3a
LinkWhale <filipbjed@gmail.com>
2023-12-14 11:24:04 +0100

    23.12.14

--- script/script.js
+++ script/script.js
@@ -19,15 +19,38 @@ function classes(data) {
     <details id="${data.results[i].name}1" class="layer1">
     <summary>${data.results[i].name}</summary>
         <details class="description">
+            <summary>Proficiencies</summary>
+            <strong>Armor: </strong> ${format_string(data.results[i].prof_armor)}
+            <strong>Saving throws: </strong> ${format_string(data.results[i].prof_saving_throws)}
+            <strong>Skills: </strong> ${format_string(data.results[i].prof_skills)}
+            <strong>Tools: </strong> ${format_string(data.results[i].prof_tools)}
+            <strong>Weapons: </strong> ${format_string(data.results[i].prof_weapons)}
+        </details>
+        <details class="description">
+            <summary>Equipment</summary>
+            ${format_string(data.results[i].equipment)}
+        </details>
+        <details class="description">
             <summary>Description</summary>
             ${format_string(data.results[i].desc)}
         </details>
+        <details class="description" id="archetypes${i}">
+            <summary>${data.results[i].subtypes_name}</summary>
+        </details>
         <details class="description">
             <summary>Table</summary>
-            <h1>${data.results[i].name} Level Table</h1>
-            ${makeTable(data.results[i].table)}
+            <div><h1>${data.results[i].name} Level Table</h1>${makeTable(data.results[i].table)}</div>
         </details>
-    </details>`
+    </details>`;
+        for(let c = 0; c < data.results[i].archetypes.length; c++){
+            console.log(data.results[i].archetypes[c].name);
+            document.getElementById(`archetypes${i}`).innerHTML += `
+            <details>
+                <summary>${data.results[i].archetypes[c].name}</summary>
+                ${format_string(data.results[i].archetypes[c].desc)}
+            </details>
+            `
+        }
     }
 }
 
@@ -52,10 +75,17 @@ function format_string(text) {
             array[i] = array[i].replace("**", `<strong>`); //first **
             array[i] = array[i].replace("**", `</strong>`);} //second **
             
+
             //Lists made from *
             while(array[i].includes("*") && array[i].indexOf(" ") == 1) {
                 array[i] = `<li>${array[i].replace("*", "")}</li>`;
             }
+            //checking for singular * bold must be second or else the list wont work
+            while(array[i].includes("*")) {
+                array[i] = array[i].replace("*", `<strong>`); //first **
+                array[i] = array[i].replace("*", `</strong>`); //second **
+            }
+
 
             //format into a header
             while(array[i].includes("#")){ 
@@ -66,25 +96,32 @@ function format_string(text) {
             //Make div out of sentences starting with >
             if(array[i].indexOf(">") == 0)
             {
+                console.log(array[i])
                 //check for beggining of div
-                if(array[i + 1].indexOf(">") == 0 && div_counter == 0) {
-                    result += `<div class="arrowList">${array[i].replace(">", "")}`;
-                    div_counter ++;
+                if(array[i + 1] != null){
+                    if(array[i + 1].indexOf(">") == 0 && div_counter == 0) {
+                        result += `<div class="arrowList">${array[i].replace(">", "")}`;
+                        div_counter ++;
+                    }
+                    //check for div elements
+                    else if(array[i + 1].indexOf(">") == 0 && div_counter > 0) {
+                        result += `<p>${array[i].replace(">", "")}</p>`;
+                        div_counter ++;
+                    }
+                    //check for end of div
+                    else if(array[i + 1].indexOf(">") != 0 && div_counter > 0) {
+                        result += `<p>${array[i].replace(">", "")}</p></div>`;
+                        div_counter = 0;
+                    }
                 }
-                //check for div elements
-                else if(array[i + 1].indexOf(">") == 0 && div_counter > 0) {
-                    result += `<p>${array[i].replace(">", "")}</p>`;
-                    div_counter ++;
-                }
-                //check for end of div
-                else if(array[i + 1].indexOf(">") != 0 && div_counter > 0) {
-                    result += `<p>${array[i].replace(">", "")}</p></div>`;
-                    div_counter = 0;
+                else {
+                    result += `<div class="arrowList">${array[i].replace(">", "")}</div><br>`;
                 }
             }
             //table maker
             else if(array[i].includes("\(table\)")) 
             {
+                console.log(array[i].indexOf(i));
                 //markdown gods blessed me with including (table) before table elements
                 result += array[i]; //most of them are already bold so they arent added
                 result += `<table class="table">`;
@@ -104,7 +141,7 @@ function format_string(text) {
                         result += "</tr>"; //end table row
                         c ++;
                     }
-                    else if(array[i + c] == " ")
+                    else if(array[i + c] == "" || array[i + c] == " ")
                     { //skip spaces
                         c ++;
                     }
@@ -114,7 +151,7 @@ function format_string(text) {
                         break;
                     }
                 }
-                i += c - 2; // skip indexes of the table
+                i += c - 1; // skip indexes of the table
             }
 
             //format into text
@@ -149,7 +186,11 @@ function makeTable(content) {
     return result;
 }
 
-/*function roll_dice(multiplier, die) {
+/*
+
+IGNORE, STUFF FOR EXPANDING PROJECT LATER
+
+function roll_dice(multiplier, die) {
     let result = 0;
     for(let i = 0; i < multiplier; i++){
     result = Math.floor(Math.random() * die) + 1;
